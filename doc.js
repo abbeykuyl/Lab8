480d8f925f4e555e4bcf1b5ece28f0d0bc0a7309
abbeykuyl <akuyl12@gmail.com>
2024-11-14 11:06:01 -0500

    commit

---
+++ doc.js
@@ -0,0 +1,78 @@
+let firstname = "Abigail";
+console.log(firstname);
+let lastname = "Kuyl";
+console.log(lastname);
+var studentage = 22;
+console.log(studentage);
+
+
+let str = "hello there";
+console.log(str);
+var even = "true";
+console.log(even);
+var odd ="false" ;
+console.log(odd);
+const Number = 10;
+console.log(Number);
+
+
+var house = null
+
+let RollNumber = 15
+if (RollNumber > 10){
+    console.log("the roll number is greater than 10")
+}
+
+let isMember= true 
+if (isMember= true){
+    console.log("Member discount applied.")
+}
+
+const StartValue= 50
+if (StartValue> 0){console.log("true, value is positive")}
+    if(StartValue <0){console.log("false")}
+        if(StartValue == 0){ console.log("false")}
+
+        let a= 100;
+        let b=20; 
+        console.log(a==b);
+        console.log(a != b); 
+        console.log(a===b);
+        console.log(a>b)
+        console.log(a<b)
+        console.log(a>=b)
+        console.log(a<=b)
+
+    var FName = "Abbey" 
+    var LName = "Kuyl" 
+    var Age2 = 22
+    console.log("Hello",FName,LName, "you are", Age2,"years old");
+
+    const Start = 50 
+    if(Start > 0 , Start < 100){console.log("The Number is within Range ")}
+
+    var hascar ="true"
+    var haslicense ="true" 
+    var hasinsurance ="true"
+    if(hascar,haslicense,hasinsurance = "true"){ 
+        console.log("you can drive legally")
+    }
+    if(hascar, haslicense,hasinsurance = "false"){
+        console.log("check your driving eligibility")
+    }
+
+    var num = 21
+    if ( num % 2){ 
+        console.log("The number is odd")
+    }
+
+    var score = 85 
+    if( score>= 90){ 
+        console.log("Grade A")
+    }
+    if(80<score<90){ 
+        console.log("Grade B")
+    }
+    if( score<80){ 
+        console.log("Grade C")
+    }
